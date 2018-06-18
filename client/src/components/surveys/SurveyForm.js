855dda81743416102b2a2c3931c38f7f432f5159
Laurel Herting <laurel.herting@gmail.com>
2018-06-18 14:47:07 -0600

    DRY'ing up fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,3 +1,4 @@
+// SurveyForm shows a form for a user to add input
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
@@ -6,7 +7,30 @@ class SurveyForm extends Component {
   renderFields() {
     return (
       <div>
-        <Field type="text" name="title" component="input" />
+        <Field
+          label="Survey Title"
+          type="text"
+          name="title"
+          component="input"
+        />
+        <Field
+          label="Subject Line"
+          type="text"
+          name="subject"
+          component="input"
+        />
+        <Field
+          label="Email Body"
+          type="text"
+          name="body"
+          component="input"
+        />
+        <Field
+          label="Recipient List"
+          type="text"
+          name="emails"
+          component="input"
+        />
       </div>
     )
   }
