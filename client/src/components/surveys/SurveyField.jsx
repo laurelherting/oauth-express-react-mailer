ddf071679450dac5d0f045f16ae0c7b234f3aa92
Laurel Herting <laurel.herting@gmail.com>
2018-06-19 16:29:55 -0600

    wip-touched expression

--- client/src/components/surveys/SurveyField.jsx
+++ client/src/components/surveys/SurveyField.jsx
@@ -6,10 +6,8 @@ export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} style={{ marginBottom: '5px' }}/>
-      <div className="red-text" style={{ marginBottom: '20px' }}
-        {touched && error}
-      </div>
+      <input {...input} />
+      {touched && error}
     </div>
   );
 };
