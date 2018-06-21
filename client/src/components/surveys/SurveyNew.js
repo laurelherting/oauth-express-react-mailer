9fa53d3a6f2b3e727d1ec4c3db1da37540bf3ed3
Laurel Herting <laurel.herting@gmail.com>
2018-06-21 09:23:16 -0600

    wip-add back button to survey form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from '/SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
+
   render() {
     return (
       <div>
