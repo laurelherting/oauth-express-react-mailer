dce894dd4a189c85fdfe2f43a02f48281c86a462
Laurel Herting <laurel.herting@gmail.com>
2018-06-22 17:39:40 -0600

    add new file for refactoring

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+  { label: 'Survey Title', name: 'title', noValueError: 'Provide a Survey Title' },
+  { label: 'Subject Line', name: 'subject', noValueError: 'Provide a Subject' },
+  { label: 'Email Body', name: 'body', noValueError: 'Provide a Message' },
+  { label: 'Recipient List', name: 'emails', noValueError: 'Provide an Email' },
+];
