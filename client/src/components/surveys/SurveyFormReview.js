35707b008692e77b1ee8e7c31029e217583ff1ea
Laurel Herting <laurel.herting@gmail.com>
2018-06-22 13:23:08 -0600

    edit return statement to handle state-form values

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,5 +1,6 @@
 // SurveyFormReview shows users their form inputs for review
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SurveyFormReview = ({ onCancel }) => {
   return (
@@ -15,4 +16,8 @@ const SurveyFormReview = ({ onCancel }) => {
   );
 };
 
-export default SurveyFormReview;
+function mapStateToProps(state) {
+  return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
