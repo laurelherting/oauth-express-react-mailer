f8cd89096350413149a20ca21e6788be09a54035
Laurel Herting <laurel.herting@gmail.com>
2018-06-27 16:00:05 -0600

    rename list

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
   { label: 'Survey Title', name: 'title', noValueError: 'Provide a Survey Title' },
   { label: 'Subject Line', name: 'subject', noValueError: 'Provide a Subject' },
   { label: 'Email Body', name: 'body', noValueError: 'Provide a Message' },
-  { label: 'Recipient List', name: 'emails', noValueError: 'Provide an Email' },
+  { label: 'Recipient List', name: 'recipients', noValueError: 'Provide Recipients' },
 ];
