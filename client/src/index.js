412e26bd3af22bc55e8a4877d1dc4972720d81e3
Laurel Herting <laurel.herting@gmail.com>
2018-06-28 17:21:37 -0600

    add line to disable a11y warning, change order of import statement for axios

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,16 +6,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
+import axios from 'axios';
 import App from './components/App';
 import reducers from './reducers';
 
-// Development only axios helpers
-import axios from 'axios';
 window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
+
+  // eslint-disable-next-line
   <Provider store={store}><App /></Provider>,
   document.querySelector('#root')
 );
