18c07e0c8bce43ea25fff04f0d112aa940844432
Laurel Herting <laurel.herting@gmail.com>
2018-07-03 10:45:47 -0600

    updated records, execute queries

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -26,9 +26,22 @@ module.exports = (app) => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date(),
+          },
+        ).exec();
+      })
       .value();
-
-    console.log(events);
 
     res.send({});
   });
