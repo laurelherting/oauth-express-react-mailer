a9509858352650b93582627f13b45f656e235a23
Laurel Herting <laurel.herting@gmail.com>
2018-07-03 11:08:45 -0600

    wip-wire up to Redux

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -7,7 +7,7 @@ export const fetchUser = () => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = (token) => async dispatch => {
+export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
@@ -18,5 +18,11 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
 
