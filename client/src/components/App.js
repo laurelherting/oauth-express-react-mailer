95434ed09deda9bfa58a2a489c6c1e95782bea90
Laurel Herting <laurel.herting@gmail.com>
2018-07-19 16:48:35 -0600

    remove unused comment, add line to separate import statements

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
@@ -14,8 +15,6 @@ class App extends Component {
 
   render() {
     return (
-
-      // eslint-disable-next-line
       <div className="container">
         <BrowserRouter>
           <div>
